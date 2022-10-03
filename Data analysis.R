f7289909eeb61ab533b5a2c331ffcb43e4847411
Shangyuan Ye <114774102+shyye008@users.noreply.github.com>
2022-10-03 13:22:26 -0700

    Add files via upload

---
+++ Data analysis.R
@@ -0,0 +1,31 @@
+rm(list=ls())
+library(lme4)
+#############################################################
+# Read bootstrap HCA data set
+dat<-read.csv("Bootstrap data.csv")
+attach(dat)
+ID<-ID
+bmi<-ifelse(dat$bmi>=30,1,0)
+Sex<-ifelse(dat$Sex=="M",1,0)
+X_CMS<-data.frame(age,Sex,colo_asa,diabetes,bmi,closure)
+y<-ifelse(colo_SSI=="Y",1,0)
+dat_CMS<-data.frame(y,ID,X_CMS)
+
+# Calculate surgical volume for each hospital
+size<-c(table(ID))
+
+# Fit GLMM to estimate random effects mean and standard deviation
+fit_re<-glmer(y~age+Sex+colo_asa+diabetes+bmi+closure+(1|ID),data=dat_CMS,
+              family=binomial)
+alpha<-fixef(fit_re)
+sigma_alpha<-as.numeric(sqrt(summary(fit_re)$varcor[[1]]))
+
+# Fit GLM to estimate marginal model parameters
+fit_fix<-glm(y~age+Sex+colo_asa+diabetes+bmi+closure,data=dat_CMS,
+              family=binomial)
+beta_hat<-coef(fit_fix)[-1]
+
+# Calculate power, FPR, PPV, and NPV  
+set.seed(1)
+fmla<-"y~age+Sex+colo_asa+diabetes+bmi+closure"
+out<-sim_ranking(1000,fmla,ID,X_CMS,beta_hat,alpha,sigma_alpha)
---
+++ Function.R
@@ -0,0 +1,57 @@
+sim_ranking<-function(K=1000,fmla,ID,X,beta_hat,alpha,sigma_alpha){
+  # Function for estimate power, FPR, PPV, and NPV
+  # Takes the following arguments:
+  # K: Number of simulations
+  # fmla: Regression formula of the marginal model
+  # ID: Hospital indicators
+  # X: Covariates for risk adjustment 
+  # beta_hat: Estimated regression parameters
+  # alpha: Random effects mean
+  # sigma_alpha: Random effects standard deviation
+  
+  N<-length(ID);m<-length(unique(ID))
+  T_rank<-c();E_rank<-c()
+  for (i in 1:K) {
+    # Generate true ranks
+    alphai<-rnorm(m,alpha,sigma_alpha)
+    TR1<-ifelse(alphai>quantile(alphai,0.75),1,0)
+    T_rank<-rbind(T_rank,TR1)
+    
+    # Simulate outcome y
+    lr<-c();h<-1
+    for (j in unique(ID)) {
+      lrj<-alphai[h]+as.matrix(X)[ID==j,]%*%beta_hat
+      lr<-c(lr,lrj)
+      h<-h+1
+    }
+    p<-exp(lr)/(1+exp(lr))
+    y<-rbinom(N,1,p)
+    dats<-data.frame(y,ID,X)
+    
+    # Calculate SIRs and estimated ranks
+    fit<-glm(fmla,data=dats,family=binomial)
+    SIR<-c()
+    for (i in unique(ID)) {
+      dati<-dats[which(dats$ID==i),]
+      O<-sum(dati$y)
+      E<-sum(predict(fit,newdata=dati,type="response"))
+      SIR<-c(SIR,O/E)
+    }
+    sir3<-quantile(SIR,0.75)
+    PR<-ifelse(SIR>sir3,1,0)
+    E_rank<-rbind(E_rank,PR)
+  }
+  
+  # Calculate power, FPR, PPV, and NPV
+  power<-c();FPR<-c();PPV<-c();NPV<-c()
+  for (i in unique(ID)) {
+    simi<-cbind(T_rank[,i],E_rank[,i])
+    TP<-sum(ifelse(simi[,1]==1 & simi[,2]==1,1,0))
+    FP<-sum(ifelse(simi[,1]==0 & simi[,2]==1,1,0))
+    TN<-sum(ifelse(simi[,1]==0 & simi[,2]==0,1,0))
+    FN<-sum(ifelse(simi[,1]==1 & simi[,2]==0,1,0))
+    power<-c(power,TP/(TP+FN));FPR<-c(FPR,FP/(FP+TN))
+    PPV<-c(PPV,TP/(TP+FP));NPV<-c(NPV,TN/(FN+TN))
+  }
+  cbind(power,FPR,PPV,NPV)
+}
